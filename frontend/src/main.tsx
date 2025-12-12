03118c087fde8f14641c3b7e777b06a0234cd218
Jose <joseearronn@gmail.com>
2025-12-12 12:18:21 +0800

    fix: update paths to be dynamic

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import MainProvider from "./context/MainProvider";
 
+import "./global.css";
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 
